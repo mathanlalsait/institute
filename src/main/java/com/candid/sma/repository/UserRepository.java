4333538fda339cad80a083d7736076330bda19c7
Ankita Saklani <saklaniankita9@gmail.com>
2019-09-16 10:08:50 +0530

    successTest

--- src/main/java/com/candid/sma/repository/UserRepository.java
+++ src/main/java/com/candid/sma/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.candid.sma.repository;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
 import com.candid.sma.model.User;
